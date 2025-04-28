3cf097f6cc710b42f2445e6c7080e3cf2e357ecc
Carson Schraad <96265707+CarsonSchraad@users.noreply.github.com>
2025-04-27 21:27:05 -0500

    Update comm.c

--- comm.c
+++ comm.c
@@ -11,49 +11,34 @@ void auto_brake(int devid)
     // Your code here (Use Lab 02 - Lab 04 for reference)
     // Use the directions given in the project document
     uint16_t dist = 0;
-    int gpio19 = GREEN_LED;
-    int gpio22 = RED_LED;
     // read an input from lidar sensor and turn on corresponding led
-    while ('Y' == ser_read(0) && 'Y' == ser_read(0)) {
+    if ('Y' == ser_read(0) && 'Y' == ser_read(0)) {
+        
         uint8_t dist_L = ser_read(0);
         uint8_t dist_H = ser_read(0);
 
-        dist = (dist_H << 8) | dist_L;
+        dist = dist_L + dist_H * 256;
 
-        printf("%d\n", dist);
-
-        if (dist <= 60) {
-            gpio_write(gpio19, OFF);
-            //flashing red led
-            while (dist <= 60) {
-                gpio_write(gpio22, ON);
-                delay(500);
-                gpio_write(gpio22, OFF);
-                delay(500);
-                break;
-            }
-        
-        }
-
-        else if (60 < dist && dist <= 100) {
-            // red led
-            gpio_write(gpio19, OFF);
-            gpio_write(gpio22, ON);
+        if (dist > 200) {
+            gpio_write(RED_LED, OFF);
+            gpio_write(GREEN_LED, ON);
         }
 
         else if (100 < dist && dist <= 200) {
-            //yellow led
-            gpio_write(gpio19, ON);
-            gpio_write(gpio22, ON);
+            gpio_write(RED_LED, ON);
+            gpio_write(GREEN_LED, ON);
         }
-
+        else if (60 < dist && dist <= 100) {
+            gpio_write(GREEN_LED, OFF);
+            gpio_write(RED_LED, ON);
+        }
         else {
-            // green led
-            gpio_write(gpio22, OFF);
-            gpio_write(gpio19, ON);
+            gpio_write(GREEN_LED, OFF);
+            gpio_write(RED_LED, ON);
+            delay(100);
+            gpio_write(RED_LED, OFF);
+            delay(100);
         }
-
-
     }
 }
 int read_from_pi(int devid)
@@ -61,13 +46,32 @@ int read_from_pi(int devid)
     // Task-2: 
     // You code goes here (Use Lab 09 for reference)
     // After performing Task-2 at dnn.py code, modify this part to read angle values from Raspberry Pi.
-}
+    int recievedAngle;
+    char inputString[20];
 
+    if (ser_isready(devid)) {
+        ser_readline(devid, 20, inputString);
+        sscanf(inputString, "%d", &recievedAngle);
+        return recievedAngle;
+    }
+    
+}
 void steering(int gpio, int pos)
 {
     // Task-3: 
     // Your code goes here (Use Lab 05 for reference)
     // Check the project document to understand the task
+    #define SERVO_PULSE_MAX 2400
+    #define SERVO_PULSE_MIN 544
+    #define SERVO_PERIOD 20000
+
+    float pulseRate = (SERVO_PULSE_MAX - SERVO_PULSE_MIN)/180;
+    float pulsePeriod = (pulseRate * pos) + SERVO_PULSE_MIN;
+    
+    gpio_write(gpio, ON);
+    delay_usec(pulsePeriod);
+    gpio_write(gpio, OFF);
+    delay_usec(SERVO_PERIOD - pulsePeriod);
 }
 
 
@@ -95,7 +99,8 @@ int main()
 
         auto_brake(lidar_to_hifive); // measuring distance using lidar and braking
         int angle = read_from_pi(pi_to_hifive); //getting turn direction from pi
-        printf("\nangle=%d", angle) 
+        //printf("\nangle=%d", angle) 
+
         int gpio = PIN_19; 
         for (int i = 0; i < 10; i++){
             // Here, we set the angle to 180 if the prediction from the DNN is a positive angle
@@ -104,16 +109,16 @@ int main()
             // You are welcome to pass the angle values directly to the steering function.
             // If the servo function is written correctly, it should still work,
             // only the movements of the servo will be more subtle
-            if(angle>0){
-                steering(gpio, 180);
-            }
-            else {
-                steering(gpio,0);
-            }
+            //if(angle>0){
+                //steering(gpio, 180);
+            //}
+            //else {
+                //steering(gpio,0);
+            //}
             
             // Uncomment the line below to see the actual angles on the servo.
             // Remember to comment out the if-else statement above!
-            // steering(gpio, angle);
+            steering(gpio, angle);
         }
 
     }
